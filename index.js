45e7d498a6116aeee73fc64f51d6226057e63425
Erik Ivan Retana Lopez <merzelert@gmail.com>
2022-07-26 18:33:09 -0500

    es6

---
+++ index.js
@@ -0,0 +1,17 @@
+let hola = "Hola";
+let mundo = "Mundo!";
+
+let saludo = `${hola} ${mundo}`;
+
+console.log(saludo);
+
+let frase = "Esta es una frase epica \n"
++ "otra frase epica";
+
+console.log(frase);
+
+let frase2 = `Esta es una frase epica V2
+Esta es otra frase epica V2`;
+
+console.log(frase2);
+
